e6823ada7962993a70f9fa09c10ccf32c78eec3a
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-10-28 18:47:01 +0200

    [feature/k8sbridge-continue] Verzeichnis Auswahl Systemunabhängig fixed

--- docker-java-bridge-k8s/src/test/java/com/github/dockerjava/k8s/command/VersionCmdImplTest.java
+++ docker-java-bridge-k8s/src/test/java/com/github/dockerjava/k8s/command/VersionCmdImplTest.java
@@ -4,12 +4,12 @@ import com.github.dockerjava.api.model.Version;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.util.ClientBuilder;
 import io.kubernetes.client.util.KubeConfig;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.io.FileReader;
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,7 +18,9 @@ class VersionCmdImplTest {
 
     @Test
     void execLocalKubeConfigClient() throws IOException {
-        String kubeConfigPath = System.getenv("HOME") + "/.kube/config";
+
+        final String kubeConfigPath = System.getProperty("user.home")+ File.separator + ".kube"+File.separator+"config";
+
         ApiClient client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath))).build();
 
         VersionCmdImpl cmd = new VersionCmdImpl(client);
